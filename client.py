d857497f75617a7ad4b1e719f22ca78aab4886ce
WalmartDeli <payton.harmon99@gmail.com>
2021-11-09 20:39:14 -0600

    Encryption

--- client.py
+++ client.py
@@ -1,8 +1,21 @@
 from threading import Thread    # Threading support
+from cryptography.fernet import Fernet #symmetric encryption support
+from cryptography.hazmat.primitives import hashes # provide hash support
+from cryptography.hazmat.primitives.asymmetric import x25519 #key excahnge support
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+import base64                   # For key derivation
 import socket                   # Socket Programming support
+import os
 
 TARGET_ADDRESS = "0.0.0.0"      # Address of server we want to connect to
 TARGET_PORT = 1234              # Port of the server we want to connect to
+PRIV_KEY = X25519PrivateKey.generate() #X25519 Client Private Key
+PUB_KEY = PRIV_KEY.public_key() # #X25519 Client Public Key
+key = 0 # Symmentric Key
+fernetObject = 0 # Fernet Class for symmentric encryption
 
 server = socket.socket()                            # Create TCP socket
 server.connect((TARGET_ADDRESS, TARGET_PORT))       # User created socket to connection to the target server
@@ -11,9 +24,37 @@ print("Connected to: ", TARGET_ADDRESS , " at port ", TARGET_PORT, "\nTo close c
 # Handle the listening for new messages from the server
 def recvMessages():
     while True:
+        global key
+        global fernetObject
         incoming_message = server.recv(2048)        # Wait to receive upto 2 bytes of data from the server
-        incoming_message.decode()
-        print("\n", str(incoming_message)[2:-1])
+        if key == 0:
+            #Exchange Public Keys
+            peer_pub_key = x25519.X25519PublicKey.from_public_bytes(incoming_message)
+            server.send(
+                PUB_KEY.public_bytes(
+                    encoding=serialization.Encoding.Raw,
+                    format=serialization.PublicFormat.Raw
+                 )
+            )
+            shared_key = PRIV_KEY.exchange(peer_pub_key)
+
+            #Derive Fernet Key
+            salt = b"ThisIsForMatilda"
+            kdf = PBKDF2HMAC(
+                algorithm=hashes.SHA256(),
+                length=32,
+                salt=salt,
+                iterations=100000,
+                backend=default_backend()
+            )
+            derived_key = kdf.derive(shared_key)
+            key = base64.urlsafe_b64encode(derived_key)
+            fernetObject = Fernet(key)
+
+        else: 
+            message = fernetObject.decrypt(incoming_message)
+            message.decode()
+            print("\n", str(message)[2:-1])
 
 client_thread = Thread(target=recvMessages)         # Creation of the thread for new clients
 client_thread.daemon = True
@@ -25,4 +66,6 @@ while True:
     if new_message.lower() == "quit":               # The user can close their own client by typing quit
         print("\nDisconnecting")
         break
-    server.send(new_message.encode())
+    else:
+        cipher = fernetObject.encrypt(new_message.encode())
+        server.send(cipher)
--- server.py
+++ server.py
@@ -1,14 +1,26 @@
 from threading import Thread    # Threading support
+from cryptography.fernet import Fernet # symmetric encryption support
+from cryptography.hazmat.primitives import hashes # provide hash support
+from cryptography.hazmat.primitives.asymmetric import x25519 #key excahnge support
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+import base64                   # for key derivation
 import socket                   # Socket Programming support
 import select                   # Ascynchronous I/O with socket support
+import os
 
 HOST_ADDRESS = "0.0.0.0"    # Servers address (0.0.0.0 for generic access)
 HOST_PORT = 1234            # Servers port (Used in client scripts for connection to server)
+PRIV_KEY = X25519PrivateKey.generate() # X25519 Server Private Key
+PUB_KEY = PRIV_KEY.public_key() # X25519 Server Public Key
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # TCP socket creation
 server.bind((HOST_ADDRESS, HOST_PORT))                          # Bind the socket to our chosen address and port
 server.listen(4)                                                # Listen for upto 4 connections
 connected_clients = []                                          # Maintain list of currently connection clients
+client_keys = []
 print('''
     ███████╗███████╗██████╗ ██╗   ██╗███████╗██████╗ 
     ██╔════╝██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
@@ -22,26 +34,55 @@ print("Listening on ", HOST_ADDRESS, " at port ", HOST_PORT, "\n")
 
 #   Arguments: 
 #       target_client - The connection object for client to be removed
-def removeClient(target_client):
+def removeClient(target_client, client_key):
+    print(client_keys)
     if target_client in connected_clients:
         connected_clients.remove(target_client)
+        client_keys.remove(client_key)
+    print(client_keys)
 
 #   Arguments: 
 #       client_connection - The connection object for NEW client
 #       client_address - Address / Port of NEWLY connected client
 def intializeClient(client_connection, client_address):
-    welcome_message = ("> Connected as " + str(client_address[1]))
-    client_connection.send(welcome_message.encode())
+    #Exchange Public Keys
+    client_connection.send(
+        PUB_KEY.public_bytes(
+            encoding=serialization.Encoding.Raw,
+            format=serialization.PublicFormat.Raw
+        )
+    )
+    peer_pub_key_bytes = client_connection.recv(2048)
+    peer_pub_key = x25519.X25519PublicKey.from_public_bytes(peer_pub_key_bytes)
+    shared_key = PRIV_KEY.exchange(peer_pub_key)
+    
+    #Derive Fernet Key
+    salt = b"ThisIsForMatilda"
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    derived_key = kdf.derive(shared_key)
+    key = base64.urlsafe_b64encode(derived_key)
+
+    #Finish Client Initialization
     announcement_message = ("> User " + str(client_address[1]) + " connected")
     syncMessage(client_connection, announcement_message)
+    return key
 
 #   Arguments: 
 #       connection - The connection object for client
 #       connection_address - Address / Port of connected client
 def clientConnection(connection, connection_address):
-    intializeClient(connection, connection_address)
+    client_key = intializeClient(connection, connection_address)
+    client_keys.append(client_key)
+    client_fernet = Fernet(client_key)
     while True:
-        message = connection.recv(2048) # Accept and receive up to 2 bytes of data from the message
+        cipher = connection.recv(2048) # Accept and receive up to 2 bytes of data from the message
+        message = client_fernet.decrypt(cipher)
         if message:     # If messages contain content, send to other clients
             message.decode()
             formatted_message = ("[" + str(connection_address[1]) + "] : " + str(message)[2:-1])
@@ -51,22 +92,24 @@ def clientConnection(connection, connection_address):
             formatted_message = ("> User " + str(connection_address[1]) + " disconnected.")
             syncMessage(connection, formatted_message)
             print(str(connection_address) + " disconnected\nConnections: " + str((len(connected_clients) - 1)))
-            removeClient(connection)
+            removeClient(connection, client_key)
             break       # Kills thread
 
 # Arguments:
 #       connected_client - Client that send new message
 #       message - Pure str of message send by connect_client
 def syncMessage(connected_client, message):
-    for client in connected_clients:
-        if client == connected_client:      # Skip messaging client that sent original message
+    for x in range(len(connected_clients)):
+        if connected_clients[x] == connected_client:      # Skip messaging client that sent original message
             continue
         else:
             try:        # Send every other client the message
-                client.send(message.encode())
+                client_fernet = Fernet(client_keys[x])
+                cipher = client_fernet.encrypt(message.encode())
+                connected_clients[x].send(cipher)
             except:     # The target client can't receive the message (maybe disconnected)
-                client.close()
-                removeClient(client) 
+                connected_clients[x].close()
+                removeClient(connected_clients[x], client_keys[x]) 
 
 while True:
     client_connection, client_addr = server.accept()                                        # Accept new connections
